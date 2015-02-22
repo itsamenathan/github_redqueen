2e2f9804a28e18f355243b9ae974ecc1b5c2b6b7
Nathan W <nathan@frcv.net>
2015-02-22 16:47:50 -0600

    cleaned up a bit?

--- index.js
+++ index.js
@@ -6,26 +6,37 @@ var gitio      = require('gitio2');
 var github = new Github();
 var server = require('./server.js')(github);
 
-var pusher;
-var repo;
-var url;    
-var compare;
-var commitmsg;
-
+var push = {};
 github.on('push', function(req){
-  //console.log(req.params);
+  console.log(req.params);
 
   var payload   = req.params;
+  push.pusher = payload.pusher.name;
+  push.repo = payload.repository.full_name;
+  push.channel = payload.channel;
+  push.commitmsg = payload.head_commit.message;
+  push.numcommits = payload.commits.length;
 
-  pusher    = payload.pusher.name;
-  repo      = payload.repository.full_name;
-  url       = payload.head_commit.url;
-  commitmsg = payload.head_commit.message;
-  gitio(url, print);
+  gitio(payload.head_commit.url, print);
+
+});
+
+github.on('all', function(req){
+  var payload   = req.params;
+  if ( payload.zen ){
+    var msg = util.format("Now monitoring %s for changes.", payload.repository.full_name);
+    console.log(msg);
+    rq.toIrc(msg, '##rqtest');
+  }
 });
 
 function print(err, shorturl){
-   var msg = util.format("Detected change to %s from user %s.  Reason: '%s' - %s", repo, pusher, commitmsg, shorturl);
+   if ( push.numcommits > 1 ) {
+     var msg = util.format("Detected %s changes to %s from user %s.  Latest Reason: '%s' - %s", push.numcommits, push.repo, push.pusher, push.commitmsg, shorturl);
+   }
+   else {
+     var msg = util.format("Detected change to %s from user %s.  Reason: '%s' - %s", push.repo, push.pusher, push.commitmsg, shorturl);
+    }
    console.log(msg);
    rq.toIrc(msg, '##rqtest');
 }
