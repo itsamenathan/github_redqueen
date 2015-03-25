db207d873844640adbbd4e0a334c87f8ac82cee8
Nathan W <nathan@frcv.net>
2015-03-24 23:30:35 -0500

    some formatting

--- libs/github_hooks.js
+++ libs/github_hooks.js
@@ -13,26 +13,26 @@ function sendmsg(msg, channel){
 }
 
 github.on('push', function(req){
-  var p   = req.params;
+  var p = req.params;
 
   gitio(p.compare, function(err, shorturl){
     var msg;
     if ( p.commits.length > 1 ) {
       msg = format("{repo} - {user} - '{commitmsg}' - ({changes} changes) - {url}",{
-                   changes   : p.commits.length,
-                   repo      : p.repository.full_name,
-                   user      : p.pusher.name,
-                   commitmsg : p.head_commit.message,
-                   url       : shorturl
-                   });
+                    changes   : p.commits.length,
+                    repo      : p.repository.full_name,
+                    user      : p.pusher.name,
+                    commitmsg : p.head_commit.message,
+                    url       : shorturl
+                  });
     }
     else {
       msg = format("{repo} - {user} - '{commitmsg}' - {url}",{
-                   repo      : p.repository.full_name,
-                   user      : p.pusher.name,
-                   commitmsg : p.head_commit.message,
-                   url       : shorturl
-                   });
+                    repo      : p.repository.full_name,
+                    user      : p.pusher.name,
+                    commitmsg : p.head_commit.message,
+                    url       : shorturl
+                  });
     }
   
     sendmsg(msg, p.channel);
@@ -42,20 +42,20 @@ github.on('push', function(req){
 github.on('ping', function(req){
   var p = req.params;
   var msg = format("Now monitoring {repo} for changes.",{
-                   repo : p.repository.full_name
-                   });
+                    repo : p.repository.full_name
+                  });
   rq.toIrc(msg, p.channel);
 });
 
 github.on('pull_request', function(req){
-  var p   = req.params;
+  var p = req.params;
 
   msg = format("{repo} - {user} - 'Pull Request: {action}' - {url}",{
-                   action    : p.action,
-                   repo      : p.pull_request.user.login,
-                   user      : p.sender.login,
-                   url       : p.pull_request.url
-  });
+                action    : p.action,
+                repo      : p.pull_request.user.login,
+                user      : p.sender.login,
+                url       : p.pull_request.url
+              });
 
   sendmsg(msg, p.channel);
 });
@@ -64,11 +64,11 @@ github.on('release', function(req){
   var p = req.params;
   gitio(p.release.url, function(err, shorturl){
     msg = format("{repo} - {user} - 'Released: {tag_name}' - {url}",{
-                     repo      : p.repository.full_name,
-                     user      : p.release.author.name,
-                     tag_name  : p.release.tag_name,
-                     url       : shorturl
-    });
+                  repo      : p.repository.full_name,
+                  user      : p.release.author.name,
+                  tag_name  : p.release.tag_name,
+                  url       : shorturl
+                });
 
     sendmsg(msg, p.channel);
   });
